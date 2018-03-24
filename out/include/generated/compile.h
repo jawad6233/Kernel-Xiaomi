81c781b6dbcebc0595cd6e17445b0a50ee3502a5
Vvr_RockStar <thisisvvr@gmail.com>
2018-03-24 18:09:13 +0530

    reverts

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 20 16:13:24 IST 2018"
-#define LINUX_COMPILE_BY "vishnu"
-#define LINUX_COMPILE_HOST "Inspiron-5558"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
--- out/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.18.100-RockStar-g24a8d2c-dirty"
